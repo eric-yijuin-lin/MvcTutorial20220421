8aa41aff098a786df5f41c26d3e3bbb3cfa2b8a2
Eric Lin <eric.yijuin.lin@gmail.com>
2022-04-27 10:55:13 +0800

    add search page

--- MvcTutorial20220421/Controllers/TbHeroesController.cs
+++ MvcTutorial20220421/Controllers/TbHeroesController.cs
@@ -149,6 +149,57 @@ namespace MvcTutorial20220421.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        //GET
+        public IActionResult Search()
+        {
+            ViewData["Message"] = "英雄搜尋 GET => 取得表單";
+            return View(new HeroSearchViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Search([Bind("Name,MinAtk,MaxAtk")] HeroSearchParams searchParams)
+        {
+            
+            var viewModel = new HeroSearchViewModel(); // 清出記憶體空間 => 打掃房間
+            if (ModelState.IsValid)
+            {
+                var searchResult =  _context.TbHeroes.ToList(); // 取出所有資料
+                if (!string.IsNullOrEmpty(searchParams.Name)) // 如果有輸入名字，就用名字當條件搜尋
+                {
+                    searchResult = searchResult
+                        .Where(x => x.Name == searchParams.Name)
+                        .ToList();
+                }
+
+                // 如果攻擊力的範圍合邏輯，就用加攻擊力條件再多篩一次
+                if (searchParams.MinAtk >= 0
+                    && searchParams.MaxAtk > 0
+                    && searchParams.MinAtk < searchParams.MaxAtk)
+                {
+                    // 最小 ATK = 10；最大 ATK = 20
+                    // ==> 那些英雄攻擊歷介在 10~20 之間
+                    searchResult = searchResult
+                        .Where(x => x.Atk >= searchParams.MinAtk && x.Atk <= searchParams.MaxAtk)
+                        .ToList();
+                }
+                else // 如果攻擊力的範圍不合邏輯，就顯示忽略攻擊力條件字樣
+                {
+                    ViewData["Message"] = $"（忽略攻擊力搜尋條件）";
+                }
+
+                // 用 ViewData 讓 Controller 與 View 共享資料
+                ViewData["Message"] += $"搜尋到 {searchResult.Count} 個英雄";
+
+                // 把搜尋條件與搜尋結果賦值到 ViewModel 的 property
+                // MVC 才有辦法幫我們把資料打包進 Search.cshtml
+                viewModel.SearchParams = searchParams;
+                viewModel.Heroes = searchResult;
+            }
+            // 回傳 View + ViewModel 進行打包作業
+            return View(viewModel);
+        }
+
         private bool TbHeroExists(int id)
         {
             return _context.TbHeroes.Any(e => e.Id == id);
---
+++ MvcTutorial20220421/Models/HeroSearchViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace MvcTutorial20220421.Models
+{
+    public class HeroSearchViewModel
+    {
+        // 搜尋條件
+        public HeroSearchParams SearchParams { get; set; }
+        // 搜尋結果(英雄們)
+        public List<TbHero> Heroes { get; set; }
+
+        public HeroSearchViewModel()
+        {
+            // 初始化記憶體 => 把房間打掃乾淨，才能入住
+            SearchParams = new HeroSearchParams();
+            Heroes = new List<TbHero>();
+        }
+    }
+
+    public class HeroSearchParams
+    {
+        public string? Name { get; set; }
+        public int MinAtk { get; set; }
+        public int MaxAtk { get; set; }
+    }
+}
